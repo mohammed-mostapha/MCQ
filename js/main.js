8c2f3a48c7cd52f7bd2687532fa3b487eee7faa1
mohamed mostapha <mohamed.mostapha3699@gmail.com>
2019-10-22 12:32:42 -0700

    another update

--- js/main.js
+++ js/main.js
@@ -29,114 +29,204 @@ function toggleSidebar() {
 
 
 
-/* chapter14 */
+/* Physics 1 */
 
 function result() {
-    var question1 = document.FMchapter14.question1.value,
-     question2 = document.FMchapter14.question2.value,
-     question3 = document.FMchapter14.question3.value,
-     question4 = document.FMchapter14.question4.value,
-     question5 = document.FMchapter14.question5.value,
-     question6 = document.FMchapter14.question6.value,
-     question7 = document.FMchapter14.question7.value,
-     question8 = document.FMchapter14.question8.value,
-     question9 = document.FMchapter14.question9.value,
-     question10 = document.FMchapter14.question10.value,
+    var question1 = document.physics1form.question1.value,
+    question2 = document.physics1form.question2.value,
+    question3 = document.physics1form.question3.value,
+    question4 = document.physics1form.question4.value,
+    question5 = document.physics1form.question5.value,
+    question6 = document.physics1form.question6.value,
+    question7 = document.physics1form.question7.value,
+    question8 = document.physics1form.question8.value,
+    question9 = document.physics1form.question9.value,
+    question10 = document.physics1form.question10.value,
+    question11 = document.physics1form.question11.value,
+    question12 = document.physics1form.question12.value,
+    question13 = document.physics1form.question13.value,
+    question14 = document.physics1form.question14.value,
+    question15 = document.physics1form.question15.value,
+    question16 = document.physics1form.question16.value,
+    question17 = document.physics1form.question17.value,
+    question18 = document.physics1form.question18.value,
+    question19 = document.physics1form.question19.value,
+    question20 = document.physics1form.question20.value,
  
     correct = 0;
 
-    if( question1 == "600 000 N/m<sup>2</sup>" ) {
+    if( question1 == "the distance traveled by light in a vacuum" ) {
         correct++;
         document.querySelector('.q1').innerHTML = "1) " +  question1 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q1').innerHTML = "1) " + question1 + " <i class='fas fa-times'></i>" + "<br>" +  "  600 000 N/m<sup>2</sup>" + " <i class='fas fa-check'></i>"+ "<br>"
+        document.querySelector('.q1').innerHTML = "1) " + question1 + " <i class='fas fa-times'></i>" + "<br>" +  "  the distance traveled by light in a vacuum" + " <i class='fas fa-check'></i>"+ "<br>"
     }
 
-    if( question2 == "1 800 ATM" ) {
+    if( question2 == "the frequency of radiation emitted by cesium aroms" ) {
         correct++;
         document.querySelector('.q2').innerHTML = "2) " + question2 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q2').innerHTML = "2) " +  question2 + " <i class='fas fa-times'></i>" + "<br>" + "  1 800 ATM" + " <i class='fas fa-check'></i>" + "<br>"
+        document.querySelector('.q2').innerHTML = "2) " +  question2 + " <i class='fas fa-times'></i>" + "<br>" + "  the frequency of radiation emitted by cesium aroms" + " <i class='fas fa-check'></i>" + "<br>"
 
     }
 
-    if( question3 == "0.050" ) {
+    if( question3 == "the mass of a particular object kept in France" ) {
         correct++;
         document.querySelector('.q3').innerHTML = "3) " +  question3 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q3').innerHTML = "3) " +   question3 + " <i class='fas fa-times'></i>" + "<br>" + "  0.050" + " <i class='fas fa-check'></i>" + "<br>"
+        document.querySelector('.q3').innerHTML = "3) " +   question3 + " <i class='fas fa-times'></i>" + "<br>" + "  the mass of a particular object kept in France" + " <i class='fas fa-check'></i>" + "<br>"
 
     }
 
-    if( question4 == "1.8 x 10<sup>7</sup> kg/m<sup>3" ) {
+    if( question4 == "less than 125" ) {
         correct++;
         document.querySelector('.q4').innerHTML = "4) " +   question4 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q4').innerHTML = "4) " +   question4 + " <i class='fas fa-times'></i>" + "<br>" + "  1.8 x 10<sup>7</sup> kg/m<sup>3" + " <i class='fas fa-check'></i>" + "<br>"
+        document.querySelector('.q4').innerHTML = "4) " +   question4 + " <i class='fas fa-times'></i>" + "<br>" + "  less than 125" + " <i class='fas fa-check'></i>" + "<br>"
 
     }
 
-    if( question5 == "1.41 x 10<sup>-3</sup> kg/m<sup>3</sup>" ) {
+    if( question5 == "greater than 15" ) {
         correct++;
         document.querySelector('.q5').innerHTML = "5) " +  question5 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q5').innerHTML = "5) " +  question5 + " <i class='fas fa-times'></i>" + "<br>" +  "  1.41 x 10<sup>-3</sup> kg/m<sup>3</sup>" + " <i class='fas fa-check'></i>" + "<br>"
+        document.querySelector('.q5').innerHTML = "5) " +  question5 + " <i class='fas fa-times'></i>" + "<br>" +  "  greater than 15" + " <i class='fas fa-check'></i>" + "<br>"
 
     }
 
-    if( question6 == "20.4 ATM" ) {
+    if( question6 == "false" ) {
         correct++;
         document.querySelector('.q6').innerHTML = "6) " + question6 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q6').innerHTML = "6) " + question6 + " <i class='fas fa-times'></i>" + "<br>" +  "  20.4 ATM" + " <i class='fas fa-check'></i>"+ "<br>"
+        document.querySelector('.q6').innerHTML = "6) " + question6 + " <i class='fas fa-times'></i>" + "<br>" +  "  false" + " <i class='fas fa-check'></i>"+ "<br>"
 
     }
 
-    if( question7 == "100 ATM" ) {
+    if( question7 == "1.3" ) {
         correct++;
         document.querySelector('.q7').innerHTML = "7) " + question7 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q7').innerHTML = "7) " + question7 + " <i class='fas fa-times'></i>" + "<br>" +  "  100 ATM" + " <i class='fas fa-check'></i>"+ "<br>"
+        document.querySelector('.q7').innerHTML = "7) " + question7 + " <i class='fas fa-times'></i>" + "<br>" +  "  1.3" + " <i class='fas fa-check'></i>"+ "<br>"
 
     }
 
-    if( question8 == "5.3 x 10<sup>18</sup>kg" ) {
+    if( question8 == "4.8 x 10<sup>-4</sup>" ) {
         correct++;
         document.querySelector('.q8').innerHTML = "8) " + question8 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q8').innerHTML = "8) " + question8 + " <i class='fas fa-times'></i>" + "<br>" +  "  5.3 x 10<sup>18</sup>kg" + " <i class='fas fa-check'></i>"+ "<br>"
+        document.querySelector('.q8').innerHTML = "8) " + question8 + " <i class='fas fa-times'></i>" + "<br>" +  "  4.8 x 10<sup>-4</sup>" + " <i class='fas fa-check'></i>"+ "<br>"
 
     }
 
-    if( question9 == "214 kg" ) {
+    if( question9 == "0.348" ) {
         correct++;
         document.querySelector('.q9').innerHTML = "9) " + question9 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q9').innerHTML = "9) " + question9 + " <i class='fas fa-times'></i>" + "<br>" +  "  214 kg" + " <i class='fas fa-check'></i>" + "<br>" 
+        document.querySelector('.q9').innerHTML = "9) " + question9 + " <i class='fas fa-times'></i>" + "<br>" +  "  0.348" + " <i class='fas fa-check'></i>" + "<br>" 
 
     }
 
-    if( question10 == "89%" ) {
+    if( question10 == "4 x 10<sup>-5</sup>cm" ) {
         correct++;
         document.querySelector('.q10').innerHTML = "10) " + question10 + " <i class='fas fa-check'></i>";
     }else {
-        document.querySelector('.q10').innerHTML = "10) " + question10 + " <i class='fas fa-times'></i>" + "<br>" +  "  89%" + " <i class='fas fa-check'></i>" + "<br>" 
+        document.querySelector('.q10').innerHTML = "10) " + question10 + " <i class='fas fa-times'></i>" + "<br>" +  "  4 x 10<sup>-5</sup>cm" + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question11 == "3.5 x 10<sup>2</sup>" ) {
+        correct++;
+        document.querySelector('.q11').innerHTML = "11) " + question11 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q11').innerHTML = "11) " + question11 + " <i class='fas fa-times'></i>" + "<br>" +  "  3.5 x 10<sup>2</sup>" + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question12 == "7.26 x 10<sup>3</sup> kg/m<sup>3</sup>" ) {
+        correct++;
+        document.querySelector('.q12').innerHTML = "12) " + question12 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q12').innerHTML = "12) " + question12 + " <i class='fas fa-times'></i>" + "<br>" +  "  7.26 x 10<sup>3</sup> kg/m<sup>3</sup>" + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question13 == 0.0012 ) {
+        correct++;
+        document.querySelector('.q13').innerHTML = "13) " + question13 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q13').innerHTML = "13) " + question13 + " <i class='fas fa-times'></i>" + "<br>" +  ' ' + 0.0012 + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question14 == 0.0000735 ) {
+        correct++;
+        document.querySelector('.q14').innerHTML = "14) " + question14 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q14').innerHTML = "14) " + question14 + " <i class='fas fa-times'></i>" + "<br>" + ' ' + 0.0000735 + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question15 == "1.776 x 10<sup>-4</sup>" ) {
+        correct++;
+        document.querySelector('.q15').innerHTML = "15) " + question15 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q15').innerHTML = "15) " + question15 + " <i class='fas fa-times'></i>" + "<br>" +  " 1.776 x 10<sup>-4</sup>" + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question16 == "3.25 x 10<sup>-10</sup>" ) {
+        correct++;
+        document.querySelector('.q16').innerHTML = "16) " + question16 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q16').innerHTML = "16) " + question16 + " <i class='fas fa-times'></i>" + "<br>" +  "  3.25 x 10<sup>-10</sup>" + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question17 == "4 significant figures" ) {
+        correct++;
+        document.querySelector('.q17').innerHTML = "17) " + question17 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q17').innerHTML = "17) " + question17 + " <i class='fas fa-times'></i>" + "<br>" +  "  4 significant figures" + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question18 == 0.91 ) {
+        correct++;
+        document.querySelector('.q18').innerHTML = "1) " + question18 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q18').innerHTML = "18) " + question18 + " <i class='fas fa-times'></i>" + "<br>" +  " " + 0.91 + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question19 == 1133 ) {
+        correct++;
+        document.querySelector('.q19').innerHTML = "19) " + question19 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q19').innerHTML = "19) " + question19 + " <i class='fas fa-times'></i>" + "<br>" +  " " + 1133 + " <i class='fas fa-check'></i>" + "<br>" 
+
+    }
+
+    if( question20 == 7.6 ) {
+        correct++;
+        document.querySelector('.q20').innerHTML = "20) " + question20 + " <i class='fas fa-check'></i>";
+    }else {
+        document.querySelector('.q20').innerHTML = "20) " + question20 + " <i class='fas fa-times'></i>" + "<br>" +  " " + 7.6 + " <i class='fas fa-check'></i>" + "<br>" 
 
     }
 
     var messages = ["Great Job!", "That's just okay", "You really need to do better..."],
-        images = ["images/success.gif","images/passed.png", "images/mr.bean fail.gif"],
+        images = ["../images/success.gif","../images/passed.png", "../images/mr.bean fail.gif"],
         range;
 
-        if ( correct < 5 ) {
+        if ( correct < 14 ) {
             range = 2;
         }
 
-        if ( correct > 4 && correct < 10 ) {
+        if ( correct >= 14 && correct < 20 ) {
             range = 1;
         }
 
-        if ( correct > 9 ) {
+        if ( correct > 19 ) {
             range = 0;
         }
     
@@ -159,7 +249,7 @@ $('.evaluator').click(function() {
 
 
 var counter = 0,
-    timeLeft = 60;
+    timeLeft = 1800;
 
 
 function pad2(number) {   
@@ -179,7 +269,7 @@ function convertSeconds(s) {
 var ding;
 
 function timerFinished() {
-    ding = new Audio('ding.mp3');
+    ding = new Audio('../audio/ding.mp3');
     ding.play();
 }
 
@@ -210,7 +300,7 @@ function setup() {
 
 
 
-/* end chapter14 */
+/* end physics 1 */
 
 
 
